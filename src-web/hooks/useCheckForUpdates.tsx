21e8f453d2c6df9420f6c18ba1cda2b0ee90bd65
Song <1667077010@qq.com>
2025-07-13 11:21:22 +0000

    disable in-app update

--- src-web/hooks/useCheckForUpdates.tsx
+++ src-web/hooks/useCheckForUpdates.tsx
@@ -4,23 +4,24 @@ import { showAlert } from '../lib/alert';
 import { appInfo } from '../lib/appInfo';
 import { minPromiseMillis } from '../lib/minPromiseMillis';
 import { invokeCmd } from '../lib/tauri';
+import { Link } from '../components/core/Link';
 
 export function useCheckForUpdates() {
   return useMutation({
     mutationKey: ['check_for_updates'],
     mutationFn: async () => {
-      const hasUpdate: boolean = await minPromiseMillis(invokeCmd('cmd_check_for_updates'), 500);
-      if (!hasUpdate) {
+      // const hasUpdate: boolean = await minPromiseMillis(invokeCmd('cmd_check_for_updates'), 500);
+      // if (!hasUpdate) {
         showAlert({
           id: 'no-updates',
-          title: 'No Update Available',
+          title: 'Disable Updates',
           body: (
             <>
-              You are currently on the latest version <InlineCode>{appInfo.version}</InlineCode>
+              This is a fork version: <InlineCode>{appInfo.version}</InlineCode>. Seek update in <Link href="https://github.com/subframe7536/yaak">GitHub</Link>.
             </>
           ),
         });
-      }
+      // }
     },
   });
 }
