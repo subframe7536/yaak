f0cae5bf47672c5a5acbb91515dc3ed87ff1659a
Song <1667077010@qq.com>
2025-07-13 08:25:44 +0000

    remove updater

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1231,7 +1231,7 @@ pub fn run() {
         .plugin(tauri_plugin_window_state::Builder::default().build())
         .plugin(tauri_plugin_deep_link::init())
         .plugin(tauri_plugin_shell::init())
-        .plugin(tauri_plugin_updater::Builder::default().build())
+        // .plugin(tauri_plugin_updater::Builder::default().build())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_os::init())
         .plugin(tauri_plugin_fs::init())
