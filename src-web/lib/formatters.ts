36e0f645ccacdfc09f392d391f05b8b3719439a1
Gregory Schier <gschier1990@gmail.com>
2025-07-18 13:27:22 -0700

    Catch jsonrepair exception

--- src-web/lib/formatters.ts
+++ src-web/lib/formatters.ts
@@ -11,9 +11,14 @@ export async function tryFormatJson(
   if (text === '') return text;
 
   // Normalize whitespace and repair JSON
-  const repairedText = options.repair
-    ? jsonrepair(text.replace(/(^|\n)\s*[\u3000\u2000-\u200F\u2028-\u202F]+/g, '$1  '))
-    : text;
+  let repairedText = text;
+  try {
+    repairedText = options.repair
+      ? jsonrepair(text.replace(/(^|\n)\s*[\u3000\u2000-\u200F\u2028-\u202F]+/g, '$1  '))
+      : text;
+  } catch {
+    // Leave unrepaired repair fails. This could be caused by {"foo": ${[ BAR ]}} template syntax
+  }
 
   try {
     const result = await invokeCmd<string>('cmd_format_json', { text: repairedText });
